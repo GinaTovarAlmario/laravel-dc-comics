66a1d32c15cde14b483a23453f88182fb2c3cb0e
GinaTovarAlmario <ginatovaralmario@gmail.com>
2024-10-30 16:49:17 +0100

    added some details to header

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,5 +1,19 @@
 <header>
-    <h1>
-        Header
-    </h1>
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+        <div class="container-fluid d-flex align-items-center">
+                <a class="navbar-brand" href="#">
+                    <img src="{{ Vite::asset('resources/img/bts-logo.png') }}" width="45" class="d-inline-block align-top" alt="">
+                </a>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Albums</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
 </header>
