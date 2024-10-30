c704c3a29f346c5f09e484625a9f7cf204c91c08
GinaTovarAlmario <ginatovaralmario@gmail.com>
2024-10-30 17:14:23 +0100

    added style to homepage

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -1,8 +1,12 @@
 @extends('layouts.app')
 
-@section("page-title", "Homepage")
-@section("main-content")
-    <h1>
-        Homepage
-    </h1>
+@section('page-title', 'Homepage')
+@section('main-content')
+    <section class="home">
+        <div class="row">
+            <div class="col-12">
+                <img src="{{ Vite::asset('resources/img/bts-jumbo.jpg') }}" class="img-fluid w-100" alt="jumbo">
+            </div>
+        </div>
+    </section>
 @endsection
--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,5 +1,9 @@
 <footer>
-    <h2>
-        Footer
-    </h2>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-12 g-0 text-center">
+                <p class="py-3 bg-dark mb-0">made with 🤓 gipa</p>
+            </div>
+        </div>
+    </div>
 </footer>
