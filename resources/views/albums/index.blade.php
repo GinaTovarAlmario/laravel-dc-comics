ac7f5032fb4099c03e799134cdb08194aeff7d2f
GinaTovarAlmario <ginatovaralmario@gmail.com>
2024-10-30 21:35:16 +0100

    added data to the table

--- resources/views/albums/index.blade.php
+++ resources/views/albums/index.blade.php
@@ -9,19 +9,24 @@
                 <table class="table table-striped">
                     <thead>
                         <tr>
-                            <th scope="col">#</th>
-                            <th scope="col">First</th>
-                            <th scope="col">Last</th>
-                            <th scope="col">Handle</th>
+                            <th scope="col">Title</th>
+                            <th scope="col">Date of Release</th>
+                            <th scope="col">Number of tracks</th>
+                            <th scope="col">Main track</th>
                         </tr>
                     </thead>
                     <tbody>
-                        <tr>
-                            <th scope="row">1</th>
-                            <td>Mark</td>
-                            <td>Otto</td>
-                            <td>@mdo</td>
-                        </tr>
+                        @forelse ($albums as $album)
+                            <tr>
+                                <th scope="row">{{$album->title}}</th>
+                                <td>{{$album->date_release}}</td>
+                                <td>{{$album->number_of_tracks}}</td>
+                                <td>{{$album->main_track}}</td>
+                            </tr>
+                        @empty
+
+                        @endforelse
+
                     </tbody>
                 </table>
             </div>
