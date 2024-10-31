49aa594e2886381b2067902bd82c4a5af2b11e65
GinaTovarAlmario <ginatovaralmario@gmail.com>
2024-10-31 16:58:49 +0100

    added helper old in the create form

--- resources/views/albums/create.blade.php
+++ resources/views/albums/create.blade.php
@@ -24,28 +24,28 @@
                 @csrf
                 <div class="mb-3">
                     <label for="title" class="form-label">Title</label>
-                    <input type="text" class="form-control" id="title" name="title">
+                    <input type="text" class="form-control" id="title" name="title" value="{{old('title')}}">
                 </div>
                 <div class="mb-3">
                     <label for="date_release" class="form-label">Date of Release</label>
                     <input type="text" class="form-control" id="date_release" name="date_release"
-                        placeholder="YYYY-MM-DD">
+                        placeholder="YYYY-MM-DD" value="{{old('date_release')}}">
                 </div>
                 <div class="mb-3">
                     <label for="img_url" class="form-label">Image Cover Url</label>
-                    <input type="text" class="form-control" id="img_url" name="img_url">
+                    <input type="text" class="form-control" id="img_url" name="img_url" value="{{old('img_url')}}">
                 </div>
                 <div class="mb-3">
                     <label for="number_of_tracks" class="form-label">Number of Tracks</label>
-                    <input type="number" class="form-control" id="number_of_tracks" name="number_of_tracks" min="1">
+                    <input type="number" class="form-control" id="number_of_tracks" name="number_of_tracks" value="{{old('number_of_tracks')}}" min="1">
                 </div>
                 <div class="mb-3">
                     <label for="main_track" class="form-label">Main Track</label>
-                    <input type="text" class="form-control" id="main_track" name="main_track">
+                    <input type="text" class="form-control" id="main_track" name="main_track" value="{{old('main_track')}}">
                 </div>
                 <div class="mb-3">
                     <label for="description" class="form-label">Brief Description</label>
-                    <textarea class="form-control" id="description" name="description" rows="8"></textarea>
+                    <textarea class="form-control" id="description" name="description" value="{{old('description')}}" rows="8"></textarea>
                 </div>
                 <div class="mb-3">
                     <button type="submit" class="btn bshow me-3">
