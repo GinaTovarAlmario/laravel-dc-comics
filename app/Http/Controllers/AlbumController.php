c8cdefc13a3ef1272201dd301295148d61c93f55
GinaTovarAlmario <ginatovaralmario@gmail.com>
2024-10-31 16:54:53 +0100

    fixed validation for img url

--- app/Http/Controllers/AlbumController.php
+++ app/Http/Controllers/AlbumController.php
@@ -35,7 +35,7 @@ class AlbumController extends Controller
         $request->validate([
             'title'=>'required|string|min:3|max:255',
             'date_release'=>'required|date',
-            'img_url'=>'required|url|max:255',
+            'img_url'=>'required|url',
             'number_of_tracks'=>'required|integer|min:1',
             'main_track'=>'required|string|min:2|max:255',
             'description'=>'required|string|min:2|max:1000',
